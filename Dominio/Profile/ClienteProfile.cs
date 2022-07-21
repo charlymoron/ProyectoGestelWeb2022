4c3143383f9ac8457d86b64d226319ede823300e
charlymoron <charlymoron@gmail.com>
2022-07-21 18:36:01 -0300

    Update ClienteProfile.cs

--- Dominio/Profile/ClienteProfile.cs
+++ Dominio/Profile/ClienteProfile.cs
@@ -18,15 +18,7 @@ namespace Dominio.Profile
                 .ForMember(dest => dest.RazonSocial, cfg => cfg.MapFrom(s => s.RazonSocial))
                 .ForMember(dest => dest.Activo, cfg => cfg.MapFrom(s => s.Activo == 1 ? "SI" : "NO"))
                 .ForMember(dest => dest.FechaDeAlta, cfg => cfg.MapFrom(c => c.FechaDeAlta.ToString("dd/MM/yyyy")))
-                .ForMember(dest => dest.FechaDeBaja, cfg => cfg.Ignore())
-
-                //.ForMember(dest => dest.ArchivosImportados, cfg => cfg.Ignore())
-
-                //.ForMember(dest => dest.Edificios, cfg => cfg.Ignore())
-                //.ForMember(dest => dest.Estadisticas, cfg => cfg.Ignore())
-
-                //.ForMember(dest => dest.TipoEstadisticas, cfg => cfg.Ignore())
-                ;
+                .ForMember(dest => dest.FechaDeBaja, cfg => cfg.Ignore());
         }
     }
 }
