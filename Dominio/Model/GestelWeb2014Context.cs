b36bf528f79a690602db7557b23425bfa362978a
charlymoron <charlymoron@gmail.com>
2022-07-21 20:16:38 -0300

    fixes y add configurationmanager para connection string

--- Dominio/Model/GestelWeb2014Context.cs
+++ Dominio/Model/GestelWeb2014Context.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+
+
+
 
 namespace Dominio.Model
 {
@@ -40,8 +44,12 @@ namespace Dominio.Model
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost; Database=GestelWeb2014; User Id=sa; Password=P@ssword2710");
+               // optionsBuilder.UseSqlServer("Server=localhost; Database=GestelWeb2014; User Id=sa; Password=P@ssword2710");
+
+                string connectionString = ConfigurationManager.AppSettings["GestelWeb2014"]!;
+
+                optionsBuilder.UseSqlServer(connectionString);
+
             }
         }
 
