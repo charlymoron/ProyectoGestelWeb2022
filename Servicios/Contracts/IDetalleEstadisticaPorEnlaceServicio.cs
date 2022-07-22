c65b8771d4e6f644f37b4b2cb175271cedb43630
charlymoron <charlymoron@gmail.com>
2022-07-22 13:12:18 -0300

    se agrega controller de DetalleEstadidtciaPorEnlace

---
+++ Servicios/Contracts/IDetalleEstadisticaPorEnlaceServicio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Dominio.Model;
+
+namespace Servicios.Contracts
+{
+    public interface IDetalleEstadisticaPorEnlaceServicio : IServicio<DetalleEstadisticaPorEnlace>
+    {
+
+
+
+        IEnumerable<DetalleEstadisticaPorEnlace> GetDetalleEstadisticaPorEnlaceId(long Id);
+
+    }
+}
+
---
+++ Servicios/Implem/DetalleEstadisticaPorEnlaceServicio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Contracts;
+using Dominio.Contracts;
+using Dominio.Model;
+using Servicios.Contracts;
+
+namespace Servicios.Implem
+{
+    public class DetalleEstadisticaPorEnlaceServicio : Servicio<DetalleEstadisticaPorEnlace>, IDetalleEstadisticaPorEnlaceServicio
+    {
+        private readonly IRepositorioBase<DetalleEstadisticaPorEnlace> _repositorio;
+
+        public DetalleEstadisticaPorEnlaceServicio(IRepositorioBase<DetalleEstadisticaPorEnlace> repositorio, IUnitOfWork unitOfWork) : base(repositorio, unitOfWork)
+        {
+            _repositorio = repositorio;
+        }
+
+        public IEnumerable<DetalleEstadisticaPorEnlace> GetDetalleEstadisticaPorEnlaceId(long Id)
+        {
+            var lista = _repositorio.FindByCondition(u => u.EnlaceId == Id);
+
+            return lista;
+
+        }
+    }
+}
+
